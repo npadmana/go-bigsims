22ef04da4900ddaea28cf5ced1d066d2919e07fa
Nikhil Padmanabhan <nikhil.padmanabhan@yale.edu>
2013-04-17 16:47:19 -0400

    check in...

--- db/loadfof/loadfof.go
+++ db/loadfof/loadfof.go
@@ -8,17 +8,70 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"database/sql"
 	"flag"
 	"fmt"
+	_ "github.com/npadmana/go-sqlite3"
 	"io"
 	"log"
-	//"os"
+	"math"
+	"os"
 	"os/exec"
 	"path"
+	"sync"
 )
 
+type Halo struct {
+	Tag               int64
+	Mass              float64
+	Xcen, Ycen, Zcen  float64
+	Vx, Vy, Vz, Vdisp float64
+}
+
+func SQLiteTableInserter(db *sql.DB, tablename string, ch chan Halo, blocksize int, wg *sync.WaitGroup) {
+
+	ok := true
+	var err error
+	var tx *sql.Tx
+	var stmt *sql.Stmt
+	var h Halo
+
+	for ok {
+		// Prepare the transaction
+		tx, err = db.Begin()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Prepare the statement
+		stmt, err = tx.Prepare(fmt.Sprintf("insert into %s values(?,?,?,?,?,?,?,?,?)", tablename))
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for i := 0; i < blocksize; i++ {
+			h, ok = <-ch
+			if !ok {
+				break
+			} else {
+				// Insert into the table
+				_, err = stmt.Exec(h.Tag, math.Log10(h.Mass), h.Xcen, h.Ycen, h.Zcen, h.Vx, h.Vy, h.Vz, h.Vdisp)
+				if err != nil {
+					fmt.Println(i)
+					log.Fatal(err)
+				}
+			}
+		}
+		tx.Commit()
+		stmt.Close()
+
+	}
+	wg.Done()
+
+}
+
 func main() {
-	var genericPath, fofName, dbName string
+	var genericPath, fofName, dbName, tablename string
 	var help bool
 	var err error
 
@@ -27,6 +80,7 @@ func main() {
 	flag.StringVar(&genericPath, "gio", "", "path to genericIOPrint")
 	flag.StringVar(&fofName, "fof", "", "FoF filename")
 	flag.StringVar(&dbName, "db", "", "SQLite dbname")
+	flag.StringVar(&tablename, "table", "fof", "Table name")
 	flag.Parse()
 	if help {
 		flag.Usage()
@@ -53,9 +107,35 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Set up the database
+	os.Remove(dbName)
+	db, err := sql.Open("sqlite3", dbName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create the table
+	sqls := []string{
+		fmt.Sprintf("create table %s (tag integer, logmass real, xcen real,ycen real,zcen real, vx real, vy real, vz real,vdisp real)", tablename),
+	}
+	for _, sql1 := range sqls {
+		_, err = db.Exec(sql1)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Create a channel 
+	ch := make(chan Halo)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go SQLiteTableInserter(db, tablename, ch, 100000, &wg)
+
 	// Loop over the lines, reading from the file
-	var mass, xcen, ycen, zcen, xmean, ymean, zmean, vx, vy, vz, vdisp float64
-	var hcount, htag int64
+	var xmean, ymean, zmean float64
+	var hcount int64
+	var h Halo
 	err = nil
 	var barr []byte
 	commentprefix := []byte("#")
@@ -76,14 +156,30 @@ func main() {
 		}
 
 		breader := bytes.NewBuffer(barr)
-		fmt.Fscanf(breader, "%d %d", &hcount, &htag)
-		fmt.Fscanf(breader, "%f %f %f %f", &mass, &xcen, &ycen, &zcen)
+		fmt.Fscanf(breader, "%d %d", &hcount, &h.Tag)
+		fmt.Fscanf(breader, "%f %f %f %f", &h.Mass, &h.Xcen, &h.Ycen, &h.Zcen)
 		fmt.Fscanf(breader, "%f %f %f", &xmean, &ymean, &zmean)
-		fmt.Fscanf(breader, "%f %f %f %f", &vx, &vy, &vz, &vdisp)
+		fmt.Fscanf(breader, "%f %f %f %f", &h.Vx, &h.Vy, &h.Vz, &h.Vdisp)
+		ch <- h
 
-		fmt.Println(hcount, htag, mass, Pxcen, ycen, zcen, vx, vy, vz, vdisp)
 	}
+
 	if err := cmd.Wait(); err != nil {
 		log.Fatal(err)
 	}
+
+	close(ch)
+	wg.Wait()
+
+	// Create an index on mass
+	sqls = []string{
+		fmt.Sprintf("create index massindex on %s (logmass)", tablename),
+	}
+	for _, sql1 := range sqls {
+		_, err = db.Exec(sql1)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
 }
